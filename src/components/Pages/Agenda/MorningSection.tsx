401bdb0619eed3ea513d04bab05b1f561ea5450a
damthang12 <damthangjp1201@gmail.com>
2024-11-07 10:50:31 +0700

    [UPDATE] handle agenda page

--- src/components/Pages/Agenda/MorningSection.tsx
+++ src/components/Pages/Agenda/MorningSection.tsx
@@ -1,6 +1,9 @@
 'use client';
 
-import Speaker from '@/assets/images/agenda/speaker.png';
+import Speaker1 from '@/assets/images/speakers/1.png';
+import Speaker2 from '@/assets/images/agenda/speaker.png';
+
+
 
 import Image, { StaticImageData } from "next/image";
 import { useMemo, useState } from "react";
@@ -10,106 +13,81 @@ interface Props {
   activeTab?: string;
 }
 
-export default function MorningSection({activeTab}:Props) {
+interface Speaker {
+  title: string;
+  time: string;
+  description: string;
+  image?: StaticImageData;
+}
+
+export default function MorningSection({ activeTab }: Props) {
   const [hoveredImageMorning, setHoveredImageMorning] = useState<StaticImageData | null>(null);
 
 
-
-  const listSpeakerMorning = [
+  const listSpeakerMorning : Speaker[] = [
     {
-      time: '08:30 - 09:00',
-      description: 'Guest Reception and Registration',
-      image: Speaker,
+      title: 'Opening Ceremony',
+      time: '9:00 - 9:30',
+      description: 'Opening speech by Mr. Nguyen Duy Hung - Chairman of SSI',
+      image: Speaker1,
     },
     {
-      time: '09:00 - 09:10',
-      description: 'Opening Speech. Nguyễn Duy Hưng, Chairman of SSI & SSID',
-      image: Speaker,
+      title: 'Keynote Speech on Event Theme - Tech Impact',
+      time: '9:30 - 10:15',
+      description: 'Speech by Mr. Truong Gia Binh - Chairman of FPT',
+      image: Speaker2,
     },
     {
-      time: '09:10 - 09:40',
-      description: 'Keynote: The Power of Technology for Vietnams Economy Trương Gia Bình, Chairman of FPT',
-      image: Speaker,
+      title: 'Talkshow on AI: Generative AI - Business Impact and Future Potential',
+      time: '10:15 - 11:00',
+      description:
+          'Speakers:\n' +
+          '- Vinesh - Qualcomm (online)\n' +
+          '- Lan Tran - Viettin Bank (offline)\n' +
+          '- Gaurav - JP Morgan Chase (offline)\n' +
+          '- Santhosh - Techcombank (offline)\n' +
+          '- Kavitha - AWS (online)\n' +
+          '- Moderator: Ha Dao\n',
     },
     {
-      time: '09:40 - 10:10',
-      description: 'Keynote: AI in Finance: From Vision to Practice',
-      image: Speaker,
-    },
-    {
-      time: '10:10 - 10:40',
-      description: 'Guest Reception and Registration',
-      image: Speaker,
-    },
-    {
-      time: '10:40 - 11:10',
-      description: 'Keynote: AI in Finance: From Vision to Practice',
-      image: Speaker,
-    },
-    {
-      time: '11:10 - 11:40',
-      description: 'Q&A with the Speakers',
-      image: Speaker,
-    },
-    {
-      time: '11:40 - 13:30',
-      description: 'Lunch Break',
-      image: Speaker,
-    },
+      title: 'Panel Discussion on Legal Aspects in the Crypto Field',
+      time: '11:00 - 12:00',
+      description:
+          'Moderator: Le Bao Nguyen - Deputy Director, SSI Digital\n' +
+          ' Speakers:\n' +
+          '- Malcolm - Deputy Director of Compliance, OKX\n' +
+          '- Jonathan Cheung - Head of Legal, Bybit\n' +
+          '- Legal team from Binance\n',
+    }
 
   ];
 
-  const listSpeakerMorning2 = [
+  const listSpeakerMorning2 : Speaker[] = [
     {
-      time: '08:30 - 09:00',
-      description: 'Guest Reception and Registration',
-      image: Speaker,
+      title: 'Panel on Fintech: The Future of Fintech in the Next 5 Years',
+      time: '9:00 - 10:00',
+      description: 'Panel on Fintech: The Future of Fintech in the Next 5 Years',
     },
     {
-      time: '09:00 - 09:10',
-      description: 'Opening Speech. Nguyễn Duy Hưng, Chairman of SSI & SSID',
-      image: Speaker,
+      title: 'Keynote by Mr. Hoang Nam Tien - FPT',
+      time: '10:00 - 10:30',
+      description: 'Topic: Technology\'s Impact on Education and the Future',
+
     },
     {
-      time: '09:10 - 09:40',
-      description: 'Keynote: The Power of Technology for Vietnams Economy Trương Gia Bình, Chairman of FPT',
-      image: Speaker,
-    },
-    {
-      time: '09:40 - 10:10',
-      description: 'Keynote: AI in Finance: From Vision to Practice',
-      image: Speaker,
-    },
-    {
-      time: '08:30 - 09:00',
-      description: 'Guest Reception and Registration',
-      image: Speaker,
-    },
-    {
-      time: '09:00 - 09:10',
-      description: 'Opening Speech. Nguyễn Duy Hưng, Chairman of SSI & SSID',
-      image: Speaker,
-    },
-    {
-      time: '09:10 - 09:40',
-      description: 'Keynote: The Power of Technology for Vietnams Economy Trương Gia Bình, Chairman of FPT',
-      image: Speaker,
-    },
-    {
-      time: '09:40 - 10:10',
-      description: 'Keynote: AI in Finance: From Vision to Practice',
-      image: Speaker,
+      title: 'Panel Discussion with Web 2 Investment Funds',
+      time: '10:30 - 11:30',
+      description: 'Discussion with representatives from SSIAM, Thinkzone, and Vina Capital on evaluating and investing in technology startup projects',
     },
   ];
 
   const listData = useMemo(() => {
-    if (activeTab === 'Day 1'){
+    if (activeTab === 'Day 1') {
       return listSpeakerMorning;
     }
     return listSpeakerMorning2;
 
   }, [activeTab, listSpeakerMorning, listSpeakerMorning2]);
-
 
 
   return (
@@ -123,7 +101,8 @@ export default function MorningSection({activeTab}:Props) {
 
         {/* Hovered Image - displayed only if hoveredImage is set */}
         {hoveredImageMorning && (
-            <div className="hidden tablet:block absolute left-[300px] fullscreen:left-[650px] opacity-100 transition-opacity duration-300">
+            <div
+                className="hidden tablet:block absolute left-[300px] fullscreen:left-[650px] opacity-100 transition-opacity duration-300">
               <Image
                   src={hoveredImageMorning}
                   alt="Speaker"
@@ -140,12 +119,16 @@ export default function MorningSection({activeTab}:Props) {
               <div
                   key={index}
                   className="relative group h-full flex flex-col"
-                  onMouseEnter={() => setHoveredImageMorning(speaker.image)}  // Set image on hover
-                  onMouseLeave={() => setHoveredImageMorning(null)}           // Clear image on leave
+                  onMouseEnter={() => speaker.image ? setHoveredImageMorning(speaker.image) : setHoveredImageMorning(null)}
+                  onMouseLeave={() => setHoveredImageMorning(null)}
               >
-                <div className="flex tablet:group-hover:bg-white tablet:group-hover:text-black py-6 px-2 fullscreen:py-[64px] fullscreen:text-[48px] font-neueMed">
-                  <p className=" w-full tablet:max-w-[353px] fullscreen:max-w-[941px] max-w-[116px] ">{speaker.time}</p>
-                  <p className="w-full">{speaker.description}</p>
+                <div
+                    className="flex tablet:group-hover:bg-white tablet:group-hover:text-black py-6 px-2 fullscreen:py-[64px]  text-[18px] fullscreen:text-[48px] font-neueMed">
+                  <div className=" w-full tablet:max-w-[353px] fullscreen:max-w-[941px] max-w-[116px] ">
+                    <p>{speaker.time}</p>
+                    <p className=''>{speaker.title}</p>
+                  </div>
+                  <p className='pl-5' dangerouslySetInnerHTML={{ __html: speaker.description.replaceAll('\n', '<br />') }}/>
                 </div>
                 {index !== listData.length - 1 && (
                     <hr className="w-full h-[1px] border-[#333]"/>
