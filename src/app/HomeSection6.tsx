db43b200084cdb4397c7817a546804ed69150154
lehoangvuvt <hoangvule100@gmail.com>
2024-11-27 09:23:41 +0700

    [UPDATE]: partner logos

--- src/app/HomeSection6.tsx
+++ src/app/HomeSection6.tsx
@@ -142,7 +142,7 @@ const HomeSection6 = () => {
         </div>
         <div className="w-full flex flex-wrap gap-[5px] mb-20 fullscreen:mb-32">
           <TierParners
-            partners={Array(59)
+            partners={Array(62)
               .fill("")
               .map((_, i) => {
                 return {
@@ -157,7 +157,7 @@ const HomeSection6 = () => {
         </div>
         <div className="w-full flex flex-wrap gap-[5px] mb-20 fullscreen:mb-32">
           <TierParners
-            partners={Array(58)
+            partners={Array(61)
               .fill("")
               .map((_, i) => {
                 return {
